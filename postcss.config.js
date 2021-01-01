b684ebd0a994aafae91ae391c3c2e9702b7158a5
4Azgin <doctorwhofr@protonmail.com>
2021-01-01 11:02:33 +0100

    base

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+//postcss.config.js
+module.exports = {
+  plugins: [
+    require('precss'),
+		require('tailwindcss'),
+    require('autoprefixer')
+  ]
+}
---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	
+</script>
+
+<style global lang="postcss">
+
+  /* only apply purgecss on utilities, per Tailwind docs */
+  /* purgecss start ignore */
+  @tailwind base;
+  @tailwind components;
+  /* purgecss end ignore */
+
+  @tailwind utilities;
+
+</style>
+
+
+<div class="bg-gray-900 text-white absolute top-0 right-0 h-24 w-80 flex ">
+  
+
+  <img alt="test" src="https://static.fnac-static.com/multimedia/Images/FR/NR/37/32/ae/11416119/1540-1/tsp20190923090702/L-adree.jpg" width="100" >
+  <h3 class="absolute left-32 top-5 text-xl font-bold">MARIJUANA</h3>
+  <h4 class="absolute left-32 top-12 text-lg bold">Rilès</h4>
+
+</div>
